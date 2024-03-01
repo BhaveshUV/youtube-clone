9e8620ecf2402951aa4d4fb0bd479c4f3587f6ee
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2024-03-01 19:18:29 +0530

    create btnList w functioning left&right scrollBtn

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
+    function scrollLeft() {
+        const sidepanel = document.getElementsByClassName('btnList');
+        sidepanel[0].scrollBy({ left: -250, behavior: 'smooth' });
+    }
+    function scrollRight() {
+        const sidepanel = document.getElementsByClassName('btnList');
+        sidepanel[0].scrollBy({ left: 250, behavior: 'smooth' });
+    }
+    const list = ["All", "Music", "Games", "Films", "Sports", "News", "Live", "Cricket", "Chill-out music", "Football", "Badminton", "Arts", "Speech", "Song-writing", "Quotes", "Comedy", "Acting", "Drama"];
     return (
-        <div>Buttons</div>
+        <div className="w-full relative flex items-center">
+            <button className="absolute left-0 top-2 z-10 px-4 leading-10 rounded-full bg-gray-100" onClick={scrollLeft}>&lt;</button>
+            <div className="btnList pt-3 px-10 flex gap-3 overflow-x-auto items-center">
+                {list.map((btn) => <Button name={btn} />)}
+            </div>
+            <button className="absolute right-0 top-2 z-10 px-4 leading-10 rounded-full bg-gray-100" onClick={scrollRight}>&gt;</button>
+        </div>
     );
 }
 
