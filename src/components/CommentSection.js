7012b21431c21b568a57c5eb907e7f3a82f813aa
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2024-03-06 14:06:46 +0530

    add feature to display n-level nested comments
    - create dummy n-level nested comments data (similar to the structure of og comments)
    - create a button to switch between og and n-level comments

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,29 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { YOUTUBE_COMMENT_API } from "../utils/constants";
 import CommentList from "./CommentList";
+import DUMMY_COMMENTS from "../utils/DummyComments.json";
 
 const CommentSection = ({ id }) => {
     const [comments, setComments] = useState(null);
+    const [nLevelNested, setNLevelNested] = useState(false);
 
     useEffect(() => {
         getComments();
-    }, []);
+    }, [nLevelNested]);
 
     let getComments = async () => {
-        let data = await fetch(YOUTUBE_COMMENT_API(id));
-        let dataJson = await data.json();
+        if(!nLevelNested){
+            let data = await fetch(YOUTUBE_COMMENT_API(id));
+            let dataJson = await data.json();
 
-        setComments(dataJson);
+            setComments(dataJson);
+        }else {
+            setComments(DUMMY_COMMENTS);
+        }
+
     }
 
-    if(!comments) {
+    if (!comments) {
         return null;
     }
 
     return (
         <div className="pb-4">
             <div>
-                <div className="text-lg font-bold py-4">{comments.items.length} comments</div>
+                <div className="text-lg font-bold py-4 flex items-center">
+                    <span>{comments.items.length} comments</span>
+                    <button
+                        className="font-medium text-[15px] bg-green-600 text-white rounded-full px-2 border-2 border-green-800 mx-2 active:bg-green-100 active:text-black"
+                        onClick={() => setNLevelNested(!nLevelNested)}
+                    >
+                        {nLevelNested ? "Show original comments" : "Show n-level nested comments"}
+                    </button>
+                </div>
                 <CommentList comments={comments.items} />
             </div>
         </div>
